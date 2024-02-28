c2820ff8ea0faebad65eb48584e9990808a26fef
Rodrigo Bossini <professorbossini@gmail.com>
2024-02-28 09:19:40 -0300

    t2: distancia

---
+++ t2/exercicios_002/Distancia.java
@@ -0,0 +1,9 @@
+public class Distancia{
+  public static void main(String [] args){
+    double x1 = 5;
+    double x2 = 8;
+    double y1 = 9;
+    double y2 = 4;
+    double dist = Math.sqrt((x2 - x1) * (x2 - x1) + Math.pow(y2 - y1, 2));
+  }
+}
