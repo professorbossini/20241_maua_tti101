7bde18fc94bac0ee10902b9fd4d922951a82e344
Rodrigo Bossini <professorbossini@gmail.com>
2024-02-28 12:51:15 -0300

    t1: soma

--- t1/soma/AdicaoDeDoisNumeros.java
+++ t1/soma/AdicaoDeDoisNumeros.java
@@ -1,15 +1,17 @@
-import javax.swing.*;
+import javax.swing.JOptionPane;
 public class AdicaoDeDoisNumeros{
   public static void main(String args []){
-
     //declaração de variáveis
-    double primeiroNumero;
+    double primeiroNumero;//Double
     double segundoNumero;
     double resultado;
     //entrada
-
+    //classe empacotadora(wrapper)
+    primeiroNumero = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro valor"));
+    segundoNumero = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo valor"));
     //processamento
-
+    resultado = primeiroNumero + segundoNumero;
     //saida
+    JOptionPane.showMessageDialog(null, resultado);
   }
 }
