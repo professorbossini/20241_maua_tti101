826af34d60e6fbcb652af547f1761555512b65c4
Rodrigo Bossini <professorbossini@gmail.com>
2024-03-11 09:49:26 -0300

    t3: repeticao

---
+++ t3/estruturas_de_repeticao/EstruturasDeRepeticao.java
@@ -0,0 +1,33 @@
+import static javax.swing.JOptionPane.*;
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+public class EstruturasDeRepeticao{
+  public static void main(String[] args) {
+    int numeroAlunos;
+    // do{
+    //   numeroAlunos = parseInt(showInputDialog("Quantos alunos? Digite valor positivo"));
+    // }while(numeroAlunos <= 0);
+    // showMessageDialog(null, "Ok, agora vamos começar...");
+    // double nota1, nota2;
+    // int numeroAlunos = parseInt(showInputDialog("Quantos alunos"));
+    // double mediaTotal = 0; //variavel acumuladora
+    // for(int cont = 1;cont <= numeroAlunos;cont = cont + 1){
+    //   nota1 = parseDouble(showInputDialog("Digite a primeira nota"));
+    //   nota2 = parseDouble(showInputDialog("Digite a segunda nota"));
+    //   double media = (nota1 + nota2) / 2;
+    //   showMessageDialog(null, "Média desse aluno: " + media);
+    //   mediaTotal = mediaTotal + media;
+    // }
+    // showMessageDialog(null, "Media da turma: " + mediaTotal);
+    //System.out.println(cont);
+    //while
+    // int cont = 1;
+    // while(cont <= 3){
+    //   nota1 = parseDouble(showInputDialog("Digite a primeira nota"));
+    //   nota2 = parseDouble(showInputDialog("Digite a segunda nota"));
+    //   double media = (nota1 + nota2) / 2; 
+    //   showMessageDialog(null, "Media desse aluno: " + media);
+    //   cont = cont + 1;
+    // }
+  }
+}
