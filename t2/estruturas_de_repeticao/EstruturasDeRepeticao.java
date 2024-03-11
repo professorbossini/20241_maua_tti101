e8c61428d7b4ea21bb10445c18c30d65f51f69ae
Rodrigo Bossini <professorbossini@gmail.com>
2024-03-11 11:10:25 -0300

    t2: for, while, do/while

--- t2/estruturas_de_repeticao/EstruturasDeRepeticao.java
+++ t2/estruturas_de_repeticao/EstruturasDeRepeticao.java
@@ -4,24 +4,41 @@ import static java.lang.Double.parseDouble;
 import static java.lang.Integer.parseInt;
 public class EstruturasDeRepeticao{
   public static void main(String[] args) {
-    double nota1, nota2, media;
+
+    int numeroAlunos;
+    do{
+      numeroAlunos = parseInt(showInputDialog("Digite o numero de alunos (tem que ser positivo)"));
+      if (numeroAlunos <= 0)
+        showMessageDialog(null, "Jovem, tem que ser positivo");
+    }while(numeroAlunos <= 0);   
+
+    // double nota1, nota2, media;
 
     //while
     //repetição controlada por contador
-    int cont = 1;
-    while(cont <= 3){
-      nota1 = parseDouble(showInputDialog("Digite a nota 1"));
-      nota2 = parseDouble(showInputDialog("Digite a nota 2"));
-      media = (nota1 + nota2) / 2;
-      showMessageDialog(null, "Média: " + media);
-      if(media >= 6){
-        showMessageDialog(null, "Aprovado!");
-      }
-      else{
-        showMessageDialog(null, "Prova SUB");
-      }
-      cont = cont + 1;
-    }
-    
+    //for
+    // int cont = 1;
+    // for( ; ; ){
+    //   nota1 = parseDouble(showInputDialog("Digite a nota 1"));
+    //   nota2 = parseDouble(showInputDialog("Digite a nota 2"));
+    //   media = (nota1 + nota2) / 2;
+    //   showMessageDialog(null, "Média: " + media);
+    //   if(media >= 6){
+    //     showMessageDialog(null, "Aprovado!");
+    //   }
+    //   else{
+    //     showMessageDialog(null, "Prova SUB");
+    //   }
+    //   cont++;
+    // }
+    // System.out.println(cont);
+    // int cont = 1;
+    // while(cont <= 3){
+    //   //cont = cont + 1;
+    //   // cont += 1;
+    //   // cont++;
+    //   //++cont;
+    // }
+
   }
 }
