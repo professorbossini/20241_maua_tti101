449bf2243387d8da14221e10cf21479c1b1f83dc
Rodrigo Bossini <professorbossini@gmail.com>
2024-03-21 09:41:43 -0300

    t1: repeticao

---
+++ t1/estruturas_de_repeticao/CalculoSalarioFuncionario.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class CalculoSalarioFuncionario {
+  public static void main(String[] args) {
+    var leitor = new Scanner(System.in);
+
+    System.out.println("Digite o número de funcionários");
+    var n = leitor.nextInt();
+
+    leitor.nextLine();//descarte do \n
+    System.out.println("DIgite a categoria");
+    String categoria = leitor.nextLine();
+
+
+    //1. Pedir o número de funcionários (n) e o valor do salário minimo
+
+    //2. Fazer um for que executa n vezes. Para cada vez
+
+    //3. Pegar numero de horas trabalhadas (validar com do/while, tem que ser maior que zero)
+        //turno (validar com do/while)
+        //pegar categoria (validar com do/while) 
+
+    //4. mostrar o valor da hora trabalhar de acordo com a tabela
+
+  }
+}
--- t1/estruturas_de_repeticao/VariasContinhas.java
+++ t1/estruturas_de_repeticao/VariasContinhas.java
@@ -13,14 +13,42 @@ public class VariasContinhas {
     double menor = -1;
     double mediaPares = 0;//acumulador condicional
     double percentualImpares = 0;//acumulador e contador
+
+    menor = -1;
+    //7
+    menor = 7;
+    //8
+    //5
+
+
     do{
       System.out.println("Digite um valor");
       valorDigitado = leitor.nextDouble();
       if(valorDigitado != 30000){
         soma = soma + valorDigitado;
+        quantidadeDigitados += 1; // quantidadeDigitados = quantidadeDigitados + 1
+        media = media + valorDigitado;
+        // if (maior < 0)
+        //   maior = valorDigitado;
+        // else if (valorDigitado > maior)
+        //   maior = valorDigitado;
+        if(maior < 0 || valorDigitado > maior) maior = valorDigitado;
+        //usando o operador ternário, encontre o menor, em 15 segundos
+        menor = menor == -1 || valorDigitado < menor ? valorDigitado : menor;
+        // mediaPares = valorDigitado % 2 == 0 ? mediaPares + valorDigitado  : mediaPares;
+        mediaPares = mediaPares + valorDigitado * (1 - valorDigitado % 2);
+        //percentualImpares = percentualImpares + valorDigitado % 2;
+        percentualImpares += valorDigitado % 2; 
       }
     }while(valorDigitado != 30000);
-    
+    System.out.println("Soma: " + soma);
+    System.out.println("Qtde digitados: " + quantidadeDigitados);
+    System.out.println("Media: " + (media / quantidadeDigitados));
+    System.out.println("Maior: " + maior);
+    System.out.println("Menor: " + menor);
+    System.out.println("Media pares: " + (mediaPares /(quantidadeDigitados - percentualImpares)));
+    System.out.println("Percentual impares: " + (percentualImpares / quantidadeDigitados * 100) + "%");
+    leitor.close();
   }
 }
 
