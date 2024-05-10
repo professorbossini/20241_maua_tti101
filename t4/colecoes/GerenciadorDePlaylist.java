28b919bb1b8c75237b370da8f1456a4550d122ea
Rodrigo Bossini <professorbossini@gmail.com>
2024-05-10 10:19:58 -0300

    t4: colecoes!

---
+++ t4/colecoes/GerenciadorDePlaylist.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+import javax.swing.JOptionPane;
+public class GerenciadorDePlaylist {
+  public static void main(String[] args) {
+    // List <Musica> playlist = new ArrayList<>();
+    var playlist = new ArrayList<Musica>();
+    var menu = "1-Cadastrar\n2-Avaliar\n3-Listar\n0-Sair";
+    int op;
+    do{
+      op = Integer.parseInt(JOptionPane.showInputDialog(menu));
+      switch(op){
+        case 1:{
+          //pegar o titulo junto ao usuário
+          var titulo = JOptionPane.showInputDialog("Titulo?");
+          //construir um objeto do tipo Musica
+          var musica = new Musica();
+          //atribuir o titulo à musica usando o setter correspondente
+          musica.setTitulo(titulo);
+          //usar os métodos contains e add de ArrayList e definir o método equals na classe Musica
+          //se a musica ainda não existir adicionar e confirma para o usuário
+          if(!playlist.contains(musica)){
+            playlist.add(musica);
+            JOptionPane.showMessageDialog(null, "Cadastro OK!");
+          }
+          //caso contrário, dizer que a musica ja esta cadastrada
+          else{
+            JOptionPane.showMessageDialog(null, "Música já existe!");
+          }
+          break;
+        }
+        case 2:{
+          //pedir para o usuario digitar o titulo
+          var titulo = JOptionPane.showInputDialog("Titulo");
+          //construir um objeto do tipo Musica
+          var musica = new Musica();
+          //atribuir o titulo usando o setter correspondente
+          musica.setTitulo(titulo);
+          //se a musica existir, pedir ao usuario que digite a a avaliacao
+          var indice = playlist.indexOf(musica);
+          if(indice >= 0){
+            int avaliacao;
+            //se a avaliacao estiver entre 1 e 5, avaliar e confirmar
+            //se a avaliacao estiver fora do intervalo, pedir outra ate que o usuario digite dentro do intervalo
+            do{
+              avaliacao = Integer.parseInt(
+                JOptionPane.showInputDialog("Avaliação?(1 a 5)")
+              );
+            }while (avaliacao < 1 || avaliacao > 5);
+            playlist.get(indice).setAvaliacao(avaliacao);
+            JOptionPane.showMessageDialog(null, "Avaliação OK!");
+          }
+          //se a musica nao existir, dizer que ela nao existe
+          else{
+            JOptionPane.showMessageDialog(null, "Música não está cadastrada");
+          }
+          break;
+        }
+        case 3:{
+          /*
+           * Titulo: One, Avaliação: 5
+           * **************************
+           * Titulo: Two, Avaliação: 4
+           * **************************
+           */
+          if(!playlist.isEmpty()){
+            var sb = new StringBuilder("");
+            for (Musica m : playlist){ //for each / enhanced for for m in playlist:
+              // sb.append(m.toString())  está certo mas não precisa
+              sb
+                .append(m)
+                .append("\n")
+                .append("*****************************")
+                .append("\n");
+            }
+            // JOptionPane.showMessageDialog(null, sb.toString()); não precisa chamar toString
+            JOptionPane.showMessageDialog(null, sb);
+          }
+          else
+            JOptionPane.showMessageDialog(null, "Lista vazia");
+          break;
+        }
+        case 0:{
+          JOptionPane.showMessageDialog(null, "Até logo");
+          break;
+        }
+        default:{
+          JOptionPane.showMessageDialog(
+              null, 
+              String.format("A opção %d é inválida", op)
+          );
+        }
+      }
+
+    }while(op != 0);
+  }
+}
