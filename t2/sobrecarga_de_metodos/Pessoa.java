c4b33481ec0178cdc11d5e213a807e52b43f49e3
Rodrigo Bossini <professorbossini@gmail.com>
2024-05-13 11:08:09 -0300

    t2: sobrecarga de construtores

--- t2/sobrecarga_de_metodos/Pessoa.java
+++ t2/sobrecarga_de_metodos/Pessoa.java
@@ -1,4 +1,44 @@
 public class Pessoa {
-  int idade;
+  //a expressão método construtor não existe
+  int idade = 0;
   String nome;
+
+  public void m(){
+    System.out.println("E agora?");
+    m();
+  }
+
+  public void misterio(int n){
+    //esse if detecta o caso base da recursão
+    if (n >= 0){
+      System.out.println(n);
+      //passo recursivo
+      //recursão de calda
+      misterio(n - 1);
+    }
+  }
+
+  // public Pessoa(String nome, int idade){
+  //   this(idade, nome);
+  // }
+
+  // public Pessoa(int idade, String nome) {
+  //  this(nome);
+  //  this.idade = idade;
+  // }
+
+  // public Pessoa(int idade){
+  //   this(idade, null);
+  // }
+
+  // public Pessoa(String nome){
+  //   this(0);
+  //   this.nome = nome;
+  // }
+
+    //construtor padrão
+  //é aquele que não tem parâmetro algum
+  // public Pessoa(){
+  //   System.out.println("Construindo uma nova pessoa..");
+  // }
 }
---
+++ t2/sobrecarga_de_metodos/TestePessoa.java
@@ -0,0 +1,13 @@
+public class TestePessoa {
+  public static void main(String[] args) {
+    var p = new Pessoa();
+    p.misterio(3);
+    // var p1 = 
+    //   new Pessoa(19, "Ana");
+    // var p2 = 
+    //   new Pessoa(20, "João");
+    // var p3 = new Pessoa(20);
+    // var p4 = new Pessoa("Ana");
+    
+  }
+}
